7a35441616908d6fb8246480f89d499422b76e0f
soypaduano <sebastian.paduano.m@gmail.com>
2019-09-15 15:25:38 +0200

    añadiendo libreria howler + eventos en play + generación automatica via txt de los archivos de audio

--- controller.js
+++ controller.js
@@ -1,20 +1,56 @@
-var frasesYung;
+var rapers = ["yung-beef, cecilio, pistoleros, kidkeo"]
+var currentRapper = "yung-beef";
+var soundPlayer;
+
+
+initHowler();
 
 fetch('frases.txt')
   .then(response => response.text())
   .then(text => fillFrases(text))
 
+  function initHowler(){
+  
+  }
+
 function fillFrases(frases){
     var res = frases.split("\n");
-    var sectionYungBeef = $('#section-yung-beef');
     for(var i = 0; i < res.length; i++){
-        var appendHtmlListElement = $('.list-group-item.test').clone(); //lo copio
-        //le cambio aquellos valores.
-        $(appendHtmlListElement).removeClass('test');
-        $(appendHtmlListElement).find('.frase-rapero').text(res[i]);
-        $(appendHtmlListElement).attr('id', res[i]);
-       $(sectionYungBeef).find('.list-group').append(appendHtmlListElement);
-
+      addHtmlAudioFila(res, i);
+      i++;
     }
     $('.list-group-item.test').remove();
+}
+
+function addHtmlAudioFila(res, i){
+  var sectionYungBeef = $('#section-' + currentRapper);
+  var appendHtmlListElement = $('.list-group-item.test').clone(); //lo copio
+  //le cambio aquellos valores.
+  $(appendHtmlListElement).removeClass('test');
+  $(appendHtmlListElement).find('.frase-rapero').text(res[i]);
+  $(appendHtmlListElement).attr('id', res[++i]);
+  addEventListenerPlayAudio(appendHtmlListElement);
+  $(sectionYungBeef).find('.list-group').append(appendHtmlListElement);
+}
+
+function addEventListenerPlayAudio(listElement){
+ $(listElement).find('.play-button').on('click touchstart', function () {
+
+  var audioIdToPlay = $(this).parent().attr('id');
+
+  if(soundPlayer){
+    if(soundPlayer.playing){
+      soundPlayer.pause();
+    }
+  }
+  
+  soundPlayer = new Howl({
+    src: ['audios/' + currentRapper + '/' + audioIdToPlay + '.ogg'],
+    volume: 2
+  });
+
+  soundPlayer.play();
+
+ });
+
 }
