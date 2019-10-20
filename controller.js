ae02ba938819a5b246d41c3eda8a161e4208da02
soypaduano <sebastian.paduano.m@gmail.com>
2019-10-20 20:07:25 +0200

    funcionalidades

--- controller.js
+++ controller.js
@@ -97,7 +97,6 @@ function checkSound(rapper, soundPlayer){
 }
 
 function addEventListenerDownloadAudio(listElement){
-
   $(listElement).find('.download-button').on('click touchstart', function () {
     var audioIdToPlay = $(this).parent().attr('id');
     var currentRappertoPlay = $(this).parent().parent().attr("id");
@@ -140,4 +139,16 @@ function addEventListenerShare(listElement){
         window.open('http://twitter.com/share?url='+encodeURIComponent(url)+'&text='+encodeURIComponent(text), '', 'left=0,top=0,width=550,height=450,personalbar=0,toolbar=0,scrollbars=0,resizable=0');    
       }
 });
+
 }
+
+$( document ).ready(function() {
+  $('#workWithUs').on('click touchstart', function () {
+    soundPlayer = new Howl({
+      src: ['audios/misc/gemidos.ogg'],
+      volume: 3
+    });
+    soundPlayer.play();
+    alert("Escribenos en @frasesdeltrap_app , pagamos €€€€");
+  });
+});
